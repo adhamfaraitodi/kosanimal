a18b1c3f6893f683badd957b27c3f6f28d32f9ca
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-10-17 22:51:15 +0700

    change js,add #anchor

---
+++ public/js/main.js
@@ -0,0 +1,198 @@
+(function () {
+
+    "use strict";
+
+    //===== Prealoder
+
+    window.onload = function () {
+        window.setTimeout(fadeout, 200);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+
+    /*=====================================
+    Sticky
+    ======================================= */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Easy on scroll event listener
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .page-scroll', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    //======== tiny slider
+    tns({
+        container: '.client-logo-carousel',
+        autoplay: true,
+        autoplayButtonOutput: false,
+        mouseDrag: true,
+        gutter: 15,
+        nav: false,
+        controls: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            540: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 4,
+            }
+        }
+    });
+
+
+    //WOW Scroll Spy
+    var wow = new WOW({
+        //disabled for mobile
+        mobile: false
+    });
+    wow.init();
+
+    //====== counter up
+    var cu = new counterUp({
+        start: 0,
+        duration: 2000,
+        intvalues: true,
+        interval: 100,
+        append: " ",
+    });
+    cu.start();
+
+    //======= portfolio-btn active
+    var elements = document.getElementsByClassName("portfolio-btn");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () {
+
+            // remove class from sibling
+
+            var el = elements[0];
+            while (el) {
+                if (el.tagName === "BUTTON") {
+                    //remove class
+                    el.classList.remove("active");
+
+                }
+                // pass to the new sibling
+                el = el.nextSibling;
+            }
+
+            this.classList.add("active");
+        };
+    }
+
+
+})();
+
+// ====== scroll top js
+window.onscroll = function () {
+    var header_navbar = document.querySelector(".navbar-area");
+    var sticky = header_navbar.offsetTop;
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+    } else {
+        header_navbar.classList.remove("sticky");
+    }
+    var backToTo = document.querySelector(".scroll-top");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        backToTo.style.display = "block";
+    } else {
+        backToTo.style.display = "none";
+    }
+};
+
+Math.easeInOutQuad = function (t, b, c, d) {
+
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
+
+document.querySelector('.scroll-top').onclick = function () {
+    scrollTo(document.documentElement);
+}
+
